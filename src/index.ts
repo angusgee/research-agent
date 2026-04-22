0b11d360b256fba35f52c88de4bdcb3ea9d8d8f5
AngusG <maltawebdev@gmail.com>
2026-04-23 00:31:14 +0200

    feat: get api keys from dot env

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,44 @@
 #! /user/bin/env node 
 
+import dotenv from "dotenv"
+
 function getUserInput():string {
   // get input from args
 }
 
-function parseUserInput(args:[]string):string{
+function parseUserInput(args:string[]):string{
   // validate input
 }
 
-function getAPIKeysFromEnv(){
-
+type apiKeys = {
+  openAiKey: string,
+  perplexityKey: string,
+  anthropicKey: string
 }
+
+function getAPIKeysFromEnv(): apiKeys{
+  dotenv.config();
+  const apiKeys:apiKeys = {
+    openAiKey: process.env.OPEN_API_KEY ?? "",
+    perplexityKey: process.env.PERPLEXITY_API_KEY ?? "",
+    anthropicKey: process.env.ANTHROPIC_API_KEY ?? ""
+  };
+  return apiKeys;
+}
+
+function handleMissingApiKeys(apiKeys: apiKeys): void {
+  if (apiKeys.openAiKey === "") {
+    console.log("OpenAI api key is missing, please add it")
+  }
+  if (apiKeys.perplexityKey === "") {
+    console.log("Perplexity api key is missing, please add it")
+  }
+  if (apiKeys.anthropicKey === "") {
+    console.log("Anthropic api key is missing, please add it")
+  }
+}
+
+const keys = getAPIKeysFromEnv();
+handleMissingApiKeys(keys);
 
 
