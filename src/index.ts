970020b62d198f7b811407330ae6a33ea6910620
AngusG <maltawebdev@gmail.com>
2026-04-23 10:40:30 +0200

    feat: add research using Perplexity

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node 
 
-import dotenv from "dotenv"
-import OpenAI from "openai"
+import dotenv from "dotenv";
+import OpenAI from "openai";
 
 function getUserInput():string[] {
   return process.argv;
@@ -46,13 +46,27 @@ async function callOpenAi(prompt: string):Promise<string> {
   const openAiClient = new OpenAI;
   const response = await openAiClient.responses.create({
     model: "gpt-5.4",
-    input: prompt
+    input: "Please resarch this user's question using your web search tool then return your answer and citations" + prompt
   })
   return response.output_text;
 }
 
-async function callPerpexity(prompt: string):Promise<string> {
-  return "";
+async function callPerpexity(prompt: string, perplexityApiKey: string):Promise<string> {
+  const perplexityClient = new OpenAI({
+    baseURL: "https://api.perplexity.ai",
+    apiKey: perplexityApiKey,
+  });
+  const response = await perplexityClient.chat.completions.create({
+    model: "sonar",
+    messages: [{
+      role: "user",
+      content: prompt
+    }]
+  });
+  const messageContent = response.choices[0].message.content;
+  const citations: string[] = (response as any).citations;
+  //console.log(JSON.stringify(response, null, 2));
+  return messageContent + "\n\n Citations: \n\n" + citations.join(", \n");
 }
 
 async function callAnthropic(prompt: string):Promise<string> {
@@ -67,8 +81,10 @@ if (userPrompt === undefined) {
   throw new Error("[!] No prompt provided, please pass in your prompt as the first and only argument")
 }
 
-const openAiResponse = await callOpenAi(userPrompt);
+// const openAiResponse = await callOpenAi(userPrompt);
+// console.log(openAiResponse)
 
-console.log(openAiResponse)
+const perplexityResponse = await callPerpexity(userPrompt, keys["perplexityKey"]);
+console.log(perplexityResponse);
 
 
